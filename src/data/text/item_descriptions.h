68595c0bc1025067d28f4d8841be5aacb7880224
Demonheadge <64343182+Demonheadge@users.noreply.github.com>
2024-01-30 16:20:21 +0800

    Cheese Fix

--- src/data/text/item_descriptions.h
+++ src/data/text/item_descriptions.h
@@ -2663,9 +2663,9 @@ static const u8 sSLAYER_GEM_Desc[] = _(
     "");
     
 static const u8 sCheeseDesc[] = _(
-    "Some cheese you found in\n"
-    "the Sewers. It seems like\n"
-    "it may be sentient.");
+    "Some sewer cheese.\n"
+    "It seems like it\n"
+    "may be sentient.");
     
 
 static const u8 sFrogeelBurgerDesc[] = _(
